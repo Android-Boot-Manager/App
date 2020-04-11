88e10280a64c7b23ff3f31fb78ea3fcb04b46a03
nift4 <nift4@protonmail.com>
2020-04-11 12:32:07 +0200

    Add app

---
+++ app/src/main/java/org/androidbootmanager/app/MainActivity.java
@@ -0,0 +1,14 @@
+package org.androidbootmanager.app;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
