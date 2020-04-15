59640688e6391507052b4c9c1543beccbaa149f1
nift4 <nift4@protonmail.com>
2020-04-15 16:16:43 +0200

    Update app_install.sh

--- app/src/main/assets/cp/app_install.sh
+++ app/src/main/assets/cp/app_install.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
 
 mkdir -p /sdcard/abm
-cd /data/data/org.androidbootmanager.app/assets
+cd /data/data/org.androidbootmanager.app/assets/Toolkit
 echo app_install.sh starting > /sdcard/abm/install.log
-/data/data/org.androidbootmanager.app/assets/install.sh "$1" hjacked "$2" >> /sdcard/abm/install.log 2>&1 && echo OK || echo ERROR
+/data/data/org.androidbootmanager.app/assets/Scripts/install.sh "$1" hjacked "$2" >> /sdcard/abm/install.log 2>&1 && echo OK || echo ERROR
 echo /dev/block/bootdevice/by-name/oem > /data/abm-part.cfg
 echo app_install.sh finished >> /sdcard/abm/install.log
 
