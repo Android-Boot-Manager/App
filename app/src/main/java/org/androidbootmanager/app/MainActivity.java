d85eb53f73ff29a2d062d0652e4c8627deee635a
nift4 <nift4@protonmail.com>
2020-07-16 16:44:07 +0200

    Parameter fixes

--- app/src/main/java/org/androidbootmanager/app/MainActivity.java
+++ app/src/main/java/org/androidbootmanager/app/MainActivity.java
@@ -195,7 +195,7 @@ public class MainActivity extends AppCompatActivity {
 								@Override
 								protected Object doInBackground(Object[] p1) {
 									copyAssets();
-									return doRoot(assetsdir + "/app_install.sh '" + "/data/data/org.androidbootmanager.app/files/lk2nd.img" + "' '" + romname + "'" + currentDevice);
+									return doRoot(assetsdir + "/app_install.sh '" + romname + "' " + currentDevice);
 								}
 								@Override
 								protected void onPostExecute(Object r) {
