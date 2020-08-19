417c4a3cfa932fc260db60cf0b2eaaaa467743e3
nift4 <nift4@protonmail.com>
2020-08-19 09:44:42 +0200

    Update app_is_installed.sh

--- app/src/main/assets/cp/app_is_installed.sh
+++ app/src/main/assets/cp/app_is_installed.sh
@@ -1,11 +1,7 @@
 #!/system/bin/sh
 
-noconfig() {
-/data/data/org.androidbootmanager.app/assets/Scripts/config/`cat /data/abm-codename.cfg`.sh
-}
-
 installed() {
-ls /data/abm-part.cfg /data/abm-part.2.cfg || noconfig
+ls /data/abm-part.cfg /data/abm-part.2.cfg || /data/data/org.androidbootmanager.app/assets/Scripts/config/`cat /data/abm-codename.cfg`.sh
 }
 
 /data/data/org.androidbootmanager.app/assets/Scripts/is_installed.sh && installed || rm /data/abm-part.*
