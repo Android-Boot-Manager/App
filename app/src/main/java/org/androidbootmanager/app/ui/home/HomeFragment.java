0aaabdc738fb59ff2694d32f4bfaa4caedce3dbe
nift4 <nift4@protonmail.com>
2020-10-14 17:59:48 +0200

    update android studio & gradle; fix install check

--- app/src/main/java/org/androidbootmanager/app/ui/home/HomeFragment.java
+++ app/src/main/java/org/androidbootmanager/app/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package org.androidbootmanager.app.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.topjohnwu.superuser.Shell;
@@ -29,15 +31,18 @@ public class HomeFragment extends Fragment {
         AtomicBoolean check0 = new AtomicBoolean(false);
         AtomicBoolean check1 = new AtomicBoolean(false);
         AtomicBoolean check2 = new AtomicBoolean(false);
-        boolean check3;
+        AtomicBoolean check3 = new AtomicBoolean(false);
         ImageView statusImg = root.findViewById(R.id.home_installedWorking_image);
         Shell.su(Constants.scriptDir + "is_installed.sh").submit((result) -> {
             check0.set(Shell.rootAccess());
             statusText0.setText(check0.get() ? R.string.ok : R.string.failure);
             check1.set(result.getCode() == 0);
             statusText1.setText(check1.get() ? R.string.ok : R.string.failure);
-            check2.set(result.getOut().contains("ABM.bootloader=1"));
+            check2.set(String.join("",result.getOut()).contains("ABM.bootloader=1"));
             statusText2.setText(check2.get() ? R.string.ok : R.string.failure);
+            check3.set(check1.get() && check2.get());
+            statusText3.setText(check3.get() ? R.string.ok : R.string.failure);
+            statusImg.setImageDrawable(ContextCompat.getDrawable(requireActivity(),check3.get() ? R.drawable.ic_ok : R.drawable.ic_no));
         });
         return root;
     }
