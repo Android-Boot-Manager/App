1085267a1d310bd53f67e2e950facd4d81a18c68
nift4 <nift4@protonmail.com>
2021-01-13 17:26:07 +0100

    Add FreeSpace support

--- app/src/main/java/org/androidbootmanager/app/ui/sdcard/SDCardFragment.java
+++ app/src/main/java/org/androidbootmanager/app/ui/sdcard/SDCardFragment.java
@@ -1,6 +1,5 @@
 package org.androidbootmanager.app.ui.sdcard;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -23,28 +22,28 @@ import com.topjohnwu.superuser.Shell;
 
 import org.androidbootmanager.app.R;
 import org.androidbootmanager.app.ui.home.InstalledViewModel;
-import org.androidbootmanager.app.ui.wizard.WizardActivity;
+import org.androidbootmanager.app.util.SOUtils;
 import org.jetbrains.annotations.NotNull;
 
-import java.nio.channels.AlreadyBoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class SDCardFragment extends Fragment {
 
     private InstalledViewModel model;
 
     public enum PartitionType {
-            RESERVED, ADOPTED, PORTABLE, UNKNOWN
+            RESERVED, ADOPTED, PORTABLE, UNKNOWN, FREE
     }
 
     public static class Partition {
         public PartitionType type;
         public int id;
-        public int startSector;
-        public int endSector;
-        public int size;
+        public long startSector;
+        public long endSector;
+        public long size;
         public String sizeFancy;
         public String code;
         public String name;
@@ -62,26 +61,44 @@ public class SDCardFragment extends Fragment {
                     ", name='" + name + '\'' +
                     '}';
         }
+
+        public static class FreeSpace extends Partition {
+
+            public FreeSpace(long start, long end, int bytes) {
+                startSector = start; endSector = end; size = end - start; type = PartitionType.FREE; sizeFancy = SOUtils.humanReadableByteCountBin(size * bytes);
+            }
+
+            @Override
+            public String toString() {
+                return "FreeSpace{" +
+                        "startSector=" + startSector +
+                        ", endSector=" + endSector +
+                        ", size=" + size +
+                        '}';
+            }
+        }
     }
 
     public static class SDPartitionMeta {
         public List<Partition> p = new ArrayList<>();
+        public List<Partition> u = new ArrayList<>();
         public String friendlySize;
         public String guid;
-        public int sectors;
+        public long sectors;
         public int logicalSectorSizeBytes;
         public int maxEntries;
-        public int firstUsableSector;
-        public int lastUsableSector;
-        public int alignSector;
-        public int totalFreeSectors;
+        public long firstUsableSector;
+        public long lastUsableSector;
+        public long alignSector;
+        public long totalFreeSectors;
         public String totalFreeFancy;
-        public int usableSectors;
+        public long usableSectors;
 
         @Override
         public String toString() {
             return "SDPartitionMeta{" +
                     "p=" + p +
+                    ", u=" + u +
                     ", friendlySize='" + friendlySize + '\'' +
                     ", guid='" + guid + '\'' +
                     ", sectors=" + sectors +
@@ -96,7 +113,7 @@ public class SDCardFragment extends Fragment {
                     '}';
         }
 
-        public int count() {
+        public int countPartition() {
             return p.size();
         }
 
@@ -104,6 +121,11 @@ public class SDCardFragment extends Fragment {
             return p.get(id);
         }
 
+        public int count() { return u.size(); }
+
+        public Partition dump(int id) {
+            return u.get(id);
+        }
     }
 
     public class SDRecyclerViewAdapter extends RecyclerView.Adapter<SDRecyclerViewAdapter.ViewHolder> {
@@ -124,26 +146,28 @@ public class SDCardFragment extends Fragment {
         @Override
         public void onBindViewHolder(@NotNull SDRecyclerViewAdapter.ViewHolder holder, int position) {
             Partition p;
-            for (int i = 0; i < meta.count(); i++) {
-                p = meta.dumpPartition(i);
-                switch (p.type) {
-                    case ADOPTED:
-                        holder.icon.setImageDrawable(ContextCompat.getDrawable(requireActivity(), R.drawable.ic_adopted));
-                        break;
-                    case PORTABLE:
-                        holder.icon.setImageDrawable(ContextCompat.getDrawable(requireActivity(), R.drawable.ic_sd));
-                        break;
-                    case RESERVED:
-                        holder.icon.setImageDrawable(ContextCompat.getDrawable(requireActivity(), R.drawable.ic_meta));
-                        break;
-                    case UNKNOWN:
-                    default:
-                        holder.icon.setImageDrawable(ContextCompat.getDrawable(requireActivity(), R.drawable.ic_unknown));
-                        holder.text.setText(getString(R.string.unknown_part));
-                        break;
-                }
-                holder.size.setText(p.sizeFancy);
+            p = meta.dump(position);
+            switch (p.type) {
+                case ADOPTED:
+                    holder.icon.setImageDrawable(ContextCompat.getDrawable(requireActivity(), R.drawable.ic_adopted));
+                    break;
+                case PORTABLE:
+                    holder.icon.setImageDrawable(ContextCompat.getDrawable(requireActivity(), R.drawable.ic_sd));
+                    break;
+                case RESERVED:
+                    holder.icon.setImageDrawable(ContextCompat.getDrawable(requireActivity(), R.drawable.ic_meta));
+                    break;
+                case FREE:
+                    holder.icon.setVisibility(View.INVISIBLE);
+                    holder.text.setText(R.string.free_space);
+                    break;
+                case UNKNOWN:
+                default:
+                    holder.icon.setImageDrawable(ContextCompat.getDrawable(requireActivity(), R.drawable.ic_unknown));
+                    holder.text.setText(getString(R.string.unknown_part));
+                    break;
             }
+            holder.size.setText(p.sizeFancy);
         }
 
         @Override
@@ -178,10 +202,11 @@ public class SDCardFragment extends Fragment {
             meta = new SDPartitionMeta();
         else
             return null;
+        long temp = -1;
         for (String o : r.getOut()) {
             if (o.startsWith("Disk ") && !o.contains("GUID")) {
                 String[] t = o.split(": ")[1].split(", ");
-                meta.sectors = Integer.parseInt(t[0].replace(" sectors",""));
+                meta.sectors = Long.parseLong(t[0].replace(" sectors",""));
                 meta.friendlySize = t[1];
             } else if(o.startsWith("Logical sector size: ") && o.endsWith(" bytes")) {
                 meta.logicalSectorSizeBytes = Integer.parseInt(o.replace("Logical sector size: ","").replace(" bytes",""));
@@ -190,33 +215,42 @@ public class SDCardFragment extends Fragment {
             } else if (o.startsWith("Partition table holds up to ")) {
                 meta.maxEntries = Integer.parseInt(o.replace("Partition table holds up to ","").replace(" entries",""));
             } else if (o.startsWith("First usable sector is ")) {
-                meta.firstUsableSector = Integer.parseInt(o.replace("First usable sector is ","").replaceFirst(", last usable sector is .*$",""));
-                meta.lastUsableSector = Integer.parseInt(o.replace("First usable sector is " + meta.firstUsableSector + ", last usable sector is ",""));
+                meta.firstUsableSector = Long.parseLong(o.replace("First usable sector is ","").replaceFirst(", last usable sector is .*$",""));
+                meta.lastUsableSector = Long.parseLong(o.replace("First usable sector is " + meta.firstUsableSector + ", last usable sector is ",""));
                 meta.usableSectors = meta.lastUsableSector - meta.firstUsableSector;
+                temp = meta.firstUsableSector;
             } else if (o.startsWith("Partitions will be aligned on ") && o.endsWith("-sector boundaries")) {
-                meta.alignSector = Integer.parseInt(o.replace("Partitions will be aligned on ","").replace("-sector boundaries",""));
+                meta.alignSector = Long.parseLong(o.replace("Partitions will be aligned on ","").replace("-sector boundaries",""));
             } else if (o.startsWith("Total free space is ") && o.contains("sectors")) {
                 String[] t = o.replace(")","").split("\\(");
-                meta.totalFreeSectors = Integer.parseInt(t[0].replace("Total free space is ","").replace(" sectors ",""));
+                meta.totalFreeSectors = Long.parseLong(t[0].replace("Total free space is ","").replace(" sectors ",""));
                 meta.totalFreeFancy = t[1];
             } else if(o.equals("") || o.startsWith("Number  Start (sector)    End (sector)  Size       Code  Name")) {
                 assert true; //avoid empty statement warning but do nothing
-            } else if (o.startsWith("  ")) {
-                String[] ocut = o.trim().replace("     "," ").replace("   "," ").replace("  "," ").split(" ");
+            } else if (o.startsWith("  ") && o.contains("iB")) {
+                while (o.contains("  "))
+                    o = o.trim().replace("  "," ");
+                String[] ocut = o.split(" ");
                 Partition p = new Partition();
                 p.id = Integer.parseInt(ocut[0]);
-                p.startSector = Integer.parseInt(ocut[1]);
-                p.endSector = Integer.parseInt(ocut[2]);
+                p.startSector = Long.parseLong(ocut[1]);
+                p.endSector = Long.parseLong(ocut[2]);
                 p.sizeFancy = ocut[3] + " " + ocut[4];
                 p.size = p.endSector - p.startSector;
                 p.code = ocut[5];
                 p.name = String.join(" ",Arrays.copyOfRange(ocut, 6, ocut.length));
                 p.type = PartitionType.UNKNOWN; //TODO
+                if (p.startSector > temp + meta.alignSector)
+                    meta.u.add(new Partition.FreeSpace(temp, p.startSector, meta.logicalSectorSizeBytes));
+                temp = p.endSector;
                 meta.p.add(p);
+                meta.u.add(p);
             } else {
                 return null;
             }
         }
+        if (meta.lastUsableSector > temp + meta.alignSector)
+            meta.u.add(new Partition.FreeSpace(temp, meta.lastUsableSector, meta.logicalSectorSizeBytes));
         Log.i("ABM",meta.toString());
         return meta;
     }
@@ -225,8 +259,12 @@ public class SDCardFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         model = new ViewModelProvider(requireActivity()).get(InstalledViewModel.class);
         View root = inflater.inflate(R.layout.wizard_installer_finddevice, container, false);
-        final SDPartitionMeta meta = generateMeta();
-        if (meta == null) {
+        final RecyclerView recyclerView = root.findViewById(R.id.wizard_installer_finddevice);
+        LinearLayoutManager recyclerLayoutManager = new LinearLayoutManager(requireContext());
+        recyclerView.setLayoutManager(recyclerLayoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), recyclerLayoutManager.getOrientation()));
+        AtomicReference<SDPartitionMeta> meta = new AtomicReference<>(generateMeta());
+        if (meta.get() == null) {
             if (String.join("",Shell.su("sgdisk /dev/block/mmcblk1 --print").exec().getOut()).contains("invalid GPT and valid MBR"))
                 new AlertDialog.Builder(requireActivity())
                     .setNegativeButton("Close", (d, p) -> requireActivity().finish())
@@ -239,7 +277,14 @@ public class SDCardFragment extends Fragment {
                                 .setTitle(r.isSuccess() ? R.string.successful : R.string.failed)
                                 .setMessage(String.join("\n",r.getOut()) + "\n" + String.join("\n",r.getErr()))
                                 .setCancelable(false)
-                                .setPositiveButton(R.string.ok, (d2, p2) -> requireActivity().finish())
+                                .setPositiveButton(R.string.ok, (d2, p2) -> {
+                                    if (r.isSuccess()) {
+                                        meta.set(generateMeta());
+                                        recyclerView.setAdapter(new SDRecyclerViewAdapter(meta.get()));
+                                    } else {
+                                        requireActivity().finish();
+                                    }
+                                })
                                 .show();
                     })
                     .show();
@@ -250,15 +295,8 @@ public class SDCardFragment extends Fragment {
                 .setTitle(R.string.fatal)
                 .setMessage(R.string.sd_err)
                 .show();
-        } else {
-            final RecyclerView recyclerView = root.findViewById(R.id.wizard_installer_finddevice);
-            LinearLayoutManager recyclerLayoutManager = new LinearLayoutManager(requireContext());
-            recyclerView.setLayoutManager(recyclerLayoutManager);
-            DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), recyclerLayoutManager.getOrientation());
-            recyclerView.addItemDecoration(dividerItemDecoration);
-            SDRecyclerViewAdapter recyclerViewAdapter = new SDRecyclerViewAdapter(meta);
-            recyclerView.setAdapter(recyclerViewAdapter);
-        }
+        } else
+            recyclerView.setAdapter(new SDRecyclerViewAdapter(meta.get()));
         return root;
     }
 }
