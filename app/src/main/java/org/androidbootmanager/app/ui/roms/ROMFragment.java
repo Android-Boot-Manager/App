39bcc3380afefeb61d3a86377c76b2497076c6a5
nift4 <nift4@protonmail.com>
2021-01-25 09:43:30 +0100

    Initial skeleton of the ROM tab

--- app/src/main/java/org/androidbootmanager/app/ui/roms/ROMFragment.java
+++ app/src/main/java/org/androidbootmanager/app/ui/roms/ROMFragment.java
@@ -1,31 +1,32 @@
 package org.androidbootmanager.app.ui.roms;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.topjohnwu.superuser.Shell;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.topjohnwu.superuser.io.SuFile;
 
 import org.androidbootmanager.app.R;
-import org.androidbootmanager.app.devices.DeviceModel;
-import org.androidbootmanager.app.legacy.EntryTabFragment;
-import org.androidbootmanager.app.ui.installer.DeviceInstallerWizardPageFragment;
-import org.androidbootmanager.app.ui.installer.FindDeviceWizardPageFragment;
+import org.androidbootmanager.app.ui.addrom.AddROMWelcomeWizardPageFragment;
+import org.androidbootmanager.app.ui.home.InstalledViewModel;
+import org.androidbootmanager.app.ui.wizard.WizardActivity;
 import org.androidbootmanager.app.util.ActionAbortedCleanlyError;
 import org.androidbootmanager.app.util.ConfigFile;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +37,8 @@ public class ROMFragment extends Fragment {
     RecyclerView recyclerView;
     ArrayList<Entry> entries;
     ROMRecyclerViewAdapter adapter;
+    InstalledViewModel model;
+    FloatingActionButton fab;
 
     private static class Entry {
         public String file;
@@ -57,16 +60,31 @@ public class ROMFragment extends Fragment {
             entryList = entryListIn;
         }
 
-        @NotNull
+        @NonNull
         @Override
         public ROMRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.wizard_installer_finddevice_element, parent, false);
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.entry, parent, false);
             return new ROMRecyclerViewAdapter.ViewHolder(view);
         }
 
         @Override
-        public void onBindViewHolder(@NotNull ROMRecyclerViewAdapter.ViewHolder holder, int position) {
-            //
+        public void onBindViewHolder(@NonNull ROMRecyclerViewAdapter.ViewHolder holder, int position) {
+            holder.e = entries.get(position);
+            switch (holder.e.config.get("xtype") != null ? holder.e.config.get("xtype") : "") {
+                case "UT":
+                    holder.label.setText(getString(R.string.ut));
+                    holder.pic.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ut_logo));
+                    break;
+                case "droid":
+                    holder.label.setText(getString(R.string.android));
+                    holder.pic.setImageDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.ic_roms));
+                    break;
+                default:
+                    holder.label.setText(getString(R.string.unknown_os));
+                    break;
+            }
+            if (holder.e.config.get("title") != null)
+                holder.name.setText(holder.e.config.get("title"));
         }
 
         public void updateEntries() {
@@ -80,8 +98,18 @@ public class ROMFragment extends Fragment {
 
         public class ViewHolder extends RecyclerView.ViewHolder {
 
+            ConstraintLayout container;
+            TextView label;
+            TextView name;
+            ImageView pic;
+            Entry e;
+
             public ViewHolder(View view) {
                 super(view);
+                container = view.findViewById(R.id.entry_container);
+                label = view.findViewById(R.id.entry_text);
+                pic = view.findViewById(R.id.entry_pic);
+                name = view.findViewById(R.id.entry_name);
             }
 
             public void updateEntries() {
@@ -92,7 +120,10 @@ public class ROMFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        final View root = inflater.inflate(R.layout.wizard_installer_finddevice, container, false);
+        model = new ViewModelProvider(requireActivity()).get(InstalledViewModel.class);
+        final View root = inflater.inflate(R.layout.fragment_roms, container, false);
+        fab = root.findViewById(R.id.roms_fab);
+        fab.setOnClickListener(view -> startActivity(new Intent(requireActivity(), WizardActivity.class).putExtra("codename",model.getCodename().getValue()).putExtra("StartFragment", AddROMWelcomeWizardPageFragment.class)));
         recyclerView = root.findViewById(R.id.wizard_installer_finddevice);
         LinearLayoutManager recyclerLayoutManager = new LinearLayoutManager(requireContext());
         recyclerView.setLayoutManager(recyclerLayoutManager);
@@ -105,14 +136,14 @@ public class ROMFragment extends Fragment {
 
     public void updateEntries() {
         entries = new ArrayList<>();
-        for (String entryFile : Objects.requireNonNull(SuFile.open("/data/abm/bootset/lk2nd/entries").list())) {
+        for (String entryFile : Objects.requireNonNull(SuFile.open("/data/abm/bootset/lk2nd/entries/").list())) {
             try {
-                entries.add(new Entry(entryFile));
+                entries.add(new Entry("/data/abm/bootset/lk2nd/entries/" + entryFile));
             } catch (ActionAbortedCleanlyError actionAbortedCleanlyError) {
                 actionAbortedCleanlyError.printStackTrace();
                 Toast.makeText(requireContext(), "Loading entry: Error. Action aborted cleanly.", Toast.LENGTH_LONG).show();
             }
         }
-        adapter.notifyDataSetChanged();
+        if (adapter != null) adapter.notifyDataSetChanged();
     }
 }
