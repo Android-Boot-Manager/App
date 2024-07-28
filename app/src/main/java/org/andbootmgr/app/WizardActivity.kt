6c51dfd3b9d3e0a2efac683e35370b187d8a187b
Roker2 <dimaminko2000@gmail.com>
2024-07-28 17:53:43 +0200

    WizardActivity: Remove unused import

--- app/src/main/java/org/andbootmgr/app/WizardActivity.kt
+++ app/src/main/java/org/andbootmgr/app/WizardActivity.kt
@@ -2,7 +2,6 @@ package org.andbootmgr.app
 
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
