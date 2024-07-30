f385b645494d193ce44895207ef1d39bcf466425
nift4 <nift4@protonmail.com>
2024-07-30 15:03:46 +0200

    enale proguard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
     }
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
