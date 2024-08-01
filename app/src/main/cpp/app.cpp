57e8ddc12048dfb9fbb8842c2a1bcd9917bdce36
nift4 <nift4@protonmail.com>
2024-08-01 16:05:22 +0200

    untested simulator

--- app/src/main/cpp/app.cpp
+++ app/src/main/cpp/app.cpp
@@ -1,17 +1,21 @@
-// Write C++ code here.
 //
-// Do not forget to dynamically load the C++ library into your application.
+// Created by nick on 01.08.24.
 //
-// For instance,
-//
-// In MainActivity.java:
-//    static {
-//       System.loadLibrary("app");
-//    }
-//
-// Or, in MainActivity.kt:
-//    companion object {
-//      init {
-//         System.loadLibrary("app")
-//      }
-//    }
+
+#include <jni.h>
+
+extern void simulator_start(JNIEnv* env, jobject bitmap, jint w, jint h);
+extern void simulator_stop();
+extern void simulator_key(jint key);
+
+extern "C" JNIEXPORT void JNICALL Java_org_andbootmgr_app_Simulator_key(JNIEnv* env, jobject thiz, jint key) {
+	simulator_key(key);
+}
+
+extern "C" JNIEXPORT void JNICALL Java_org_andbootmgr_app_Simulator_stop(JNIEnv* env, jobject thiz) {
+	simulator_stop();
+}
+
+extern "C" JNIEXPORT void JNICALL Java_org_andbootmgr_app_Simulator_start(JNIEnv* env, jobject thiz, jobject bitmap, jint w, jint h) {
+	simulator_start(env, bitmap, w, h);
+}
---
+++ app/src/main/java/org/andbootmgr/app/Simulator.kt
@@ -0,0 +1,30 @@
+package org.andbootmgr.app
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class Simulator : AppCompatActivity() {
+	external fun start(bitmap: Bitmap, w: Int, h: Int)
+	external fun stop()
+	external fun key(key: Int)
+	private lateinit var bitmap: Bitmap
+	private var w: Int = 0
+	private var h: Int = 0
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		w = 1080
+		h = 1920
+		bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+		setContentView(object : View(this) {
+			override fun onDraw(canvas: Canvas) {
+				super.onDraw(canvas)
+				canvas.drawBitmap(this@Simulator.bitmap, 0f, 0f, null)
+			}
+		})
+		start(bitmap, w, h)
+	}
+}
