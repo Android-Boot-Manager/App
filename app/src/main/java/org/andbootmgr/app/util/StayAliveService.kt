10159f308ad2e67ecc10d20f748fafcfa0f155aa
nift4 <nift4@protonmail.com>
2024-08-13 15:43:04 +0200

    More refactor

--- app/src/main/java/org/andbootmgr/app/util/StayAliveService.kt
+++ app/src/main/java/org/andbootmgr/app/util/StayAliveService.kt
@@ -1,24 +1,164 @@
 package org.andbootmgr.app.util
 
-import android.app.Service
+import android.annotation.SuppressLint
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Binder
 import android.os.IBinder
+import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.andbootmgr.app.R
 
-class StayAliveService : Service() {
-	override fun onCreate() {
-		super.onCreate()
-		startForeground(FG_SERVICE_ID, notif) // TODO
+interface IStayAlive {
+	fun startWork(work: suspend (Context) -> Unit, extra: Any)
+	val isWorkDone: Boolean
+	val workExtra: Any
+	fun finish()
+}
+
+class StayAliveService : LifecycleService(), IStayAlive {
+	private var work: (suspend (Context) -> Unit)? = null
+	override var isWorkDone = false
+		get() {
+			if (destroyed) {
+				throw IllegalStateException("This StayAliveService was leaked. It is already destroyed.")
+			}
+			return field
+		}
+	private var extra: Any? = null
+	override val workExtra: Any
+		get() {
+			if (destroyed) {
+				throw IllegalStateException("This StayAliveService was leaked. It is already destroyed.")
+			}
+			if (work == null) {
+				throw IllegalStateException("Tried to access work extra before work was set.")
+			}
+			return extra!!
+		}
+	private var destroyed = false
+	private var onDone: (() -> Unit)? = null
+	override fun startWork(work: suspend (Context) -> Unit, extra: Any) {
+		if (destroyed) {
+			throw IllegalStateException("This StayAliveService was leaked. It is already destroyed.")
+		}
+		if (this.work != null) {
+			throw IllegalStateException("Work already set on this StayAliveService.")
+		}
+		this.work = work
+		this.extra = extra
+		lifecycleScope.launch {
+			this@StayAliveService.work!!.invoke(this@StayAliveService)
+			isWorkDone = true
+			onDone!!.invoke()
+		}
+	}
+	override fun finish() {
+		destroyed = true
+		stopSelf()
 	}
 
-	override fun onBind(intent: Intent?): IBinder {
-		return object : Binder(), Provider {
-			override val service
-				get() = this@StayAliveService
+	override fun onCreate() {
+		super.onCreate()
+		NotificationManagerCompat.from(this).createNotificationChannel(
+			NotificationChannelCompat.Builder(SERVICE_CHANNEL,
+				NotificationManagerCompat.IMPORTANCE_HIGH)
+				.setName(getString(R.string.service_notifications))
+				.setShowBadge(true)
+				.setLightsEnabled(false)
+				.setVibrationEnabled(false)
+				.setSound(null, null)
+				.build()
+		)
+		startForeground(FG_SERVICE_ID, NotificationCompat.Builder(this, SERVICE_CHANNEL)
+			.setSmallIcon(R.drawable.abm_notif)
+			.setContentTitle(getString(R.string.abm_processing_title))
+			.setContentText(getString(R.string.abm_processing_text))
+			.setOngoing(true)
+			.setOnlyAlertOnce(true)
+			.setLocalOnly(true)
+			.build())
+		lifecycleScope.launch {
+			delay(10000)
+			// If there was nothing started after 10 seconds, there's a bug.
+			if (work == null) {
+				throw IllegalStateException("No work was submitted to StayAliveService after 10 seconds.")
+			}
 		}
 	}
 
-	interface Provider {
-		val service: StayAliveService
+	override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+		super.onStartCommand(intent, flags, startId)
+		return START_NOT_STICKY
+	}
+
+	override fun onBind(intent: Intent): IBinder {
+		super.onBind(intent)
+		if (work != null) {
+			throw IllegalStateException("Work was already set on this StayAliveService.")
+		}
+		// make sure we get promoted to started service
+		startService(Intent(this, this::class.java))
+		return object : Binder(), Provider {
+			override var service = this@StayAliveService
+			override var onDone
+				get() = this@StayAliveService.onDone!!
+				set(value) { this@StayAliveService.onDone = value }
+		}
+	}
+
+	override fun onDestroy() {
+		super.onDestroy()
+		destroyed = true
+	}
+
+	private interface Provider {
+		val service: IStayAlive
+		var onDone: () -> Unit
+	}
+
+	companion object {
+		private const val SERVICE_CHANNEL = "service"
+		private const val FG_SERVICE_ID = 1001
+		@SuppressLint("StaticFieldLeak") // application context
+		private var currentConn: StayAliveConnection? = null
+	}
+
+	class StayAliveConnection(inContext: Context,
+	                          private val onConnected: (IStayAlive) -> Unit) : ServiceConnection {
+		private val context = inContext.applicationContext
+		private var service: IStayAlive? = null
+
+		init {
+			if (currentConn != null) {
+				throw IllegalStateException("There should only be one StayAliveConnection at a time.")
+			}
+			currentConn = this
+			context.bindService(
+				Intent(context, StayAliveService::class.java),
+				this,
+				Context.BIND_IMPORTANT or Context.BIND_AUTO_CREATE
+			)
+		}
+
+		override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+			val provider = service as Provider
+			provider.onDone = {
+				context.unbindService(this)
+				currentConn = null
+			}
+			onConnected(provider.service)
+		}
+
+		override fun onServiceDisconnected(name: ComponentName?) {
+			this.service = null
+		}
 	}
 }
---
+++ app/src/main/java/org/andbootmgr/app/util/Terminal.kt
@@ -0,0 +1,173 @@
+package org.andbootmgr.app.util
+
+import android.util.Log
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.andbootmgr.app.R
+import org.andbootmgr.app.WizardActivityState
+import java.io.File
+import java.io.FileOutputStream
+
+private class BudgetCallbackList(private val log: FileOutputStream?) : MutableList<String> {
+	val internalList = ArrayList<String>()
+	var cb: ((String) -> Unit)? = null
+	override val size: Int
+		get() = internalList.size
+
+	override fun contains(element: String): Boolean {
+		return internalList.contains(element)
+	}
+
+	override fun containsAll(elements: Collection<String>): Boolean {
+		return internalList.containsAll(elements)
+	}
+
+	override fun get(index: Int): String {
+		return internalList[index]
+	}
+
+	override fun indexOf(element: String): Int {
+		return internalList.indexOf(element)
+	}
+
+	override fun isEmpty(): Boolean {
+		return internalList.isEmpty()
+	}
+
+	override fun iterator(): MutableIterator<String> {
+		return internalList.iterator()
+	}
+
+	override fun lastIndexOf(element: String): Int {
+		return internalList.lastIndexOf(element)
+	}
+
+	override fun add(element: String): Boolean {
+		onAdd(element)
+		return internalList.add(element)
+	}
+
+	override fun add(index: Int, element: String) {
+		onAdd(element)
+		return internalList.add(index, element)
+	}
+
+	override fun addAll(index: Int, elements: Collection<String>): Boolean {
+		for (i in elements) {
+			onAdd(i)
+		}
+		return internalList.addAll(index, elements)
+	}
+
+	override fun addAll(elements: Collection<String>): Boolean {
+		for (i in elements) {
+			onAdd(i)
+		}
+		return internalList.addAll(elements)
+	}
+
+	override fun clear() {
+		return internalList.clear()
+	}
+
+	override fun listIterator(): MutableListIterator<String> {
+		return internalList.listIterator()
+	}
+
+	override fun listIterator(index: Int): MutableListIterator<String> {
+		return internalList.listIterator(index)
+	}
+
+	override fun remove(element: String): Boolean {
+		return internalList.remove(element)
+	}
+
+	override fun removeAll(elements: Collection<String>): Boolean {
+		return internalList.removeAll(elements.toSet())
+	}
+
+	override fun removeAt(index: Int): String {
+		return internalList.removeAt(index)
+	}
+
+	override fun retainAll(elements: Collection<String>): Boolean {
+		return internalList.retainAll(elements.toSet())
+	}
+
+	override fun set(index: Int, element: String): String {
+		return internalList.set(index, element)
+	}
+
+	override fun subList(fromIndex: Int, toIndex: Int): MutableList<String> {
+		return internalList.subList(fromIndex, toIndex)
+	}
+
+	fun onAdd(element: String) {
+		log?.write((element + "\n").encodeToByteArray())
+		cb?.invoke(element)
+	}
+}
+
+
+/* Monospace auto-scrolling text view, fed using MutableList<String>, catching exceptions and running logic on a different thread */
+@Composable
+fun Terminal(vm: WizardActivityState, logFile: String? = null, action: suspend (MutableList<String>) -> Unit) {
+	val scrollH = rememberScrollState()
+	val scrollV = rememberScrollState()
+	val scope = rememberCoroutineScope()
+	val text = remember { mutableStateOf("") }
+	val ctx = LocalContext.current.applicationContext
+	LaunchedEffect(Unit) {
+		// TODO support resumption
+		StayAliveService.StayAliveConnection(ctx) { service ->
+			val log = logFile?.let { FileOutputStream(File(ctx.externalCacheDir, it)) }
+			// Budget CallbackList
+			val s = BudgetCallbackList(log)
+			service.startWork({
+				withContext(Dispatchers.Default) {
+					try {
+						action(s)
+					} catch (e: Throwable) {
+						s.add(ctx.getString(R.string.term_failure))
+						s.add(ctx.getString(R.string.dev_details))
+						s.add(Log.getStackTraceString(e))
+					}
+					withContext(Dispatchers.IO) {
+						log?.close()
+					}
+				}
+			}, s)
+			(service.workExtra as BudgetCallbackList).cb = { element ->
+				scope.launch {
+					text.value += element + "\n"
+					delay(200) // Give it time to re-measure
+					scrollV.animateScrollTo(scrollV.maxValue)
+					scrollH.animateScrollTo(0)
+				}
+			}
+		}
+	}
+	Text(text.value, modifier = Modifier
+		.fillMaxSize()
+		.horizontalScroll(scrollH)
+		.verticalScroll(scrollV)
+		.padding(10.dp), fontFamily = FontFamily.Monospace)
+}
